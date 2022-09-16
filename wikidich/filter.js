2f41469b497e3bf68f34e2f7e370e671733ef8f8
the void <77819937+void-kun@users.noreply.github.com>
2022-09-16 21:33:11 +0700

    Update filter.js

--- wikidich/filter.js
+++ wikidich/filter.js
@@ -1,1 +1,8 @@
-const a = document.querySelectorAll('.book-item'); Array.prototype.slice.call(document.querySelectorAll('.book-stats-box')).map((ele, i) => { if (!ele['children'][0]['children'][1].textContent.match(/[mk]/)) {a[i].remove()}});
+const a = document.querySelectorAll('.author-item'); 
+Array.prototype.slice.call(document.querySelectorAll('.stats-col'))
+.filter(ele => ele?.children.length > 0)
+.map((ele, i) => {
+  if (!ele['children'][0]['children'][1].textContent.match(/[mk]/)) {
+    a[i].remove()
+  }
+});
