11f64133247a97befe697add051fd93ea32a9666
the void <77819937+void-kun@users.noreply.github.com>
2022-12-30 16:48:57 +0700

    Update filter.js

--- wikidich/filter.js
+++ wikidich/filter.js
@@ -1,8 +1,8 @@
-const a = document.querySelectorAll('.author-item'); 
-Array.prototype.slice.call(document.querySelectorAll('.stats-col'))
-.filter(ele => ele?.children.length > 0)
-.map((ele, i) => {
-  if (!ele['children'][0]['children'][1].textContent.match(/[mk]/)) {
-    a[i].remove()
-  }
-});
+const a = document.querySelectorAll('.author-item');
+Array.prototype.slice
+  .call(document.querySelectorAll('.book-list-wrapper .stats-col'))
+  .map((ele, i) => {
+    if (!ele['children'][0]['children'][1].textContent.match(/[mk]/)) {
+      a[i].remove();
+    }
+  });
