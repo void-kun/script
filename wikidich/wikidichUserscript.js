70fe25cb7a65a387073087036d12ecd3cd64b72c
the void <77819937+void-kun@users.noreply.github.com>
2023-04-22 21:47:16 +0700

    Update wikidichUserscript.js

--- wikidich/wikidichUserscript.js
+++ wikidich/wikidichUserscript.js
@@ -4,7 +4,8 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://wikidich3.com/truyen/*
+// @match        https://wikidich6.com/truyen/*
+// @match        https://wikisach.net/truyen/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wikidich3.com
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @require      https://unpkg.com/jszip@3.1.5/dist/jszip.min.js
@@ -27,29 +28,44 @@
     let endDownload = false;
     let ebookTitle = $('.cover-info div h2').text();
     let jepub = new jEpub();
-    jepub
-      .init({
-        title: ebookTitle,
-        author: $('.cover-info div p:nth-child(3) a').text(),
-        publisher: location.host,
-        description: 'not thing',
-        tags: ['book'],
-      })
-      .uuid(location.protocol + '//' + location.host + location.pathname);
+    var chapters = [];
+    var classNames = {
+      downloadWrapper: '.control-btns',
+      downloadButton: 'btn waves-effect waves-light orange-btn',
+    };
 
-    $(document).ready(function () {
-      var chapters = $('.chapter-name')
-        .map(function () {
+    var $download = $('<a></a>', {
+      href: '#download',
+      class: 'btn waves-effect waves-light orange-btn',
+      text: 'Download',
+    });
+    $download.insertAfter('.flexbox');
+    $download.before('\r\n');
+    $download.one('click', function (e) {
+      e.preventDefault();
+      document.title = '[...] Vui lòng chờ trong giây lát';
+
+      jepub
+        .init({
+          title: ebookTitle,
+          author: $('.cover-info div p:nth-child(3) a').text(),
+          publisher: location.host,
+          description: 'not thing',
+          tags: ['book'],
+        })
+        .uuid(location.protocol + '//' + location.host + location.pathname);
+
+      this.chapters = $('.chapter-name')
+        .map(function (id) {
           var ch = $(this).find('.truncate');
           return {
             pathname: ch.attr('href'),
+            id: id + 1,
           };
         })
         .get();
-
-      var location = 'https://wikisach.net';
-      chapters = chapters.filter((chapter) => chapter !== undefined);
-      loadChapter(location, chapters);
+      this.chapters = this.chapters.filter((chapter) => chapter !== undefined);
+      loadChapter(location.origin, this.chapters);
     });
 
     function loadChapter(location, chapters) {
@@ -58,24 +74,24 @@
           saveEbook();
           return;
         }
-        let pathname = location + chapters.shift().pathname;
+        let currentChapter = chapters.shift();
         $.ajax({
-          url: pathname,
+          url: location + currentChapter.pathname,
           success: function (data) {
-            jepub.add(
-              $(data).find('.book-title').text(),
-              $(data).find('#bookContentBody').text()
-            );
-            document.title = `[${chapterList.length}] Download`;
-
+            const title = $($(data).find('.book-title')[1]).text();
+            const content = $(data).find('#bookContentBody').text();
+            jepub.add(title, content);
+            document.title = `[${currentChapter.id}] Download`;
+            console.log($(data).find('.book-title').text());
             loadChapter(location, chapters);
           },
           error: function (xhr, ajaxOptions, thrownError) {
+            console.log('Error');
             saveEbook();
             return;
           },
         });
-      }, 1000);
+      }, 3500);
     }
 
     function genEbook() {
@@ -119,5 +135,9 @@
     }
   }
 
-  main();
+  $(document).ready(function () {
+    setTimeout(function () {
+      main();
+    }, 3000);
+  });
 })(jQuery, window, document);
